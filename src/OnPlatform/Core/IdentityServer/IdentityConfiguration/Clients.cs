4c18931089d1f7c7e23c91ca58a70e37656314f9
Lakshitha Attanayaka <lakshitha.attanayaka@gmail.com>
2022-06-29 15:49:32 +0530

    OAuth2 and openID server side implementation

---
+++ src/OnPlatform/Core/IdentityServer/IdentityConfiguration/Clients.cs
@@ -0,0 +1,43 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+
+namespace IdentityServer.IdentityConfiguration
+{
+    public class Clients
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "onShoppingApi",
+                    ClientName = "ASP.NET Core OnShopping Api",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = new List<Secret> {new Secret("OnPlatform".Sha256())},
+                    AllowedScopes = new List<string> { "onShoppingApi.read" }
+                },
+                new Client
+                {
+                    ClientId = "oidcMVCApp",
+                    ClientName = "Sample ASP.NET Core MVC Web App",
+                    ClientSecrets = new List<Secret> {new Secret("OnPlatform".Sha256())},
+
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = new List<string> {"https://localhost:44346/signin-oidc"},
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "role",
+                        "onShoppingApi.read"
+                    },
+
+                    RequirePkce = true,
+                    AllowPlainTextPkce = false
+                }
+            };
+        }
+    }
+}
